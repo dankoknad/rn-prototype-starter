5a8572e84b540075eaaa9a4624157b70aeb11539
Danko Milutinovic <dankomilutinovic@gmail.com>
2017-12-06 17:02:42 +0100

    adding svg

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { Constants, Svg } from 'expo';
 
 export default class HomePage extends Component {
  static navigationOptions = {
@@ -18,6 +19,24 @@ export default class HomePage extends Component {
    return (
     <View style={styles.container}>
       <Text>You are on the Home page</Text>
+      <Svg height={50} width={120}>
+        <Svg.Circle
+          cx={25}
+          cy={25}
+          r={25}
+          strokeWidth={2.5}
+          fill="#1487ce"
+        />
+        <Svg.Text
+          fill="orange"
+          stroke="purple"
+          fontSize="30" 
+          fontWeight="bold"
+          x="90"
+          y="7"
+          textAnchor="middle"
+      >SVG</Svg.Text>
+      </Svg>
       <Button
         onPress={() => this.props.navigation.navigate('Contact')}
         title="Go to contact"
